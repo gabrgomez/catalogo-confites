1909de88b9096a9ea408dc99f88f7b48aafc55b7
Gabriela Gomez <128086087+gabrgomez@users.noreply.github.com>
2023-03-30 19:42:19 -0300

    validador actualizado

--- js/validador.js
+++ js/validador.js
@@ -1,36 +1,46 @@
-// objeto.metodo(json)
 
-
-$("#formulario1").validate({
-    rules: {
-        "txtEmail": {
-            required: true,
-            email: true
-        },
-        "txtNombre": {
-            required: true,
-            Nombre: true
-        },
-        "txtMensaje": {
-            required: true,
-            Mensaje: true
-        }
-    }, // --> Fin de reglas
-    messages: {
-        "txtEmail": {
-            required: 'Ingrese email!!!',
-            email: 'No cumple formato'
-        },
-        "txtNombre": {
-            required: 'Completa este campo !!!',
-            Nombre: 'No cumple formato'
-        },
-        "txtMensaje": {
-            required: 'Completa este campo!!!',
-            Mensaje: 'No cumple formato'
-        }
+$(document).ready(function() {
+    // Cuando se envía el formulario
+    $('#formulario').click(function(event) {
+      event.preventDefault(); // Prevenir el envío del formulario
+  
+      // Validar el nombre
+      var nombre = $('#nombre').val().trim();
+      if (nombre === '') {
+        
+        $('#nombre').focus();
+        return false;
+      }
+  
+      // Validar el correo
+      var correo = $('#correo').val().trim();
+      if (correo === '') {
+        
+        $('#correo').focus();
+        return false;
+      } else if (!validarCorreo(correo)) {
+        alert('El correo electrónico ingresado no es válido.');
+        $('#correo').focus();
+        return false;
+      }
+  
+      // Validar el mensaje
+      var mensaje = $('#mensaje').val().trim();
+      if (mensaje === '') {
+        
+        $('#mensaje').focus();
+        return false;
+      }
+  
+      // Si se pasan todas las validaciones, enviar el formulario
+      //this.submit();
+      alert("¡Mensaje enviado!");
+    });
+  
+    // Función para validar el correo electrónico
+    function validarCorreo(correo) {
+      var expresion = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+      return expresion.test(correo);
+    }
     
-    } //-->Fin de mensajes
-
-
-});
+  });
