7508f18c97fc27de878fa21c8472c8d4a239080e
lodiazb <128086248+lodiazb@users.noreply.github.com>
2023-05-07 16:21:39 -0400

    Add files via upload
    agregando js del formulario

---
+++ js/form.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+// Validación del formulario
+
+$("#login-form").validate({
+  rules: {
+    email: {
+      required: true,
+      email: true
+    },
+    password: {
+      required: true,
+      minlength: 8
+    }
+  },
+  //fin de las reglas
+  messages: {
+    email: {
+      required: "Por favor, introduzca su dirección de correo electrónico",
+      email: "Por favor, introduzca una dirección de correo electrónico válida"
+    },
+    password: {
+      required: "Por favor, introduzca su contraseña",
+      minlength: "La contraseña debe tener al menos 8 caracteres"
+    }
+  },
+  //fin de los mensajes
+  
+  
+
+
+
+
+})
+//fin del validate
+$("#btnInicioSesion").click(function (event) {
+
+
+  //   esto es para que no direccione
+    event.preventDefault();
+    
+    if ($("#login-form").valid()) {
+      alert("Inició sesion! =D");
+
+
+}
+
+
+})
+//fin del on click
+});
+//fin de doc ready
